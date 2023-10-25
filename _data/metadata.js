fefe134fcd924a3d53774f3fef7c528ad1b22428
conspirisi <66065806+conspirisi@users.noreply.github.com>
2023-10-25 19:23:36 +0100

    logo, post added, plugin removed

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,9 +2,9 @@ module.exports = {
 	title: "Faruq's blog",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Faruq's Codersguild 11ty Blog excersise ",
 	author: {
-		name: "Your Name Here",
+		name: "Faruq",
 		email: "youremailaddress@example.com",
 		url: "https://example.com/about-me/",
 	},
---
+++ content/blog/sixthpost.md
@@ -0,0 +1,7 @@
+---
+title: Faruq's Sixth Post
+date: 2023-10-23
+draft: false
+---
+
+This is Faruq additional post with some extra information in it.
