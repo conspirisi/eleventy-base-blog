851457773e64fbcf209567143a27119625ce4512
conspirisi <66065806+conspirisi@users.noreply.github.com>
2023-10-25 14:35:24 +0100

    Added Blog Title

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Faruq's blog",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Your Name Here",
 		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		url: "https://example.com/about-me/",
+	},
+};
